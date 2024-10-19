5086230b980b7b8050cd3e606799b90c0ef4210f
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-10-19 15:19:07 +0900

    Project Start

---
+++ src/main/java/sbapiserver/ddns/net/upload_server/UploadServerApplication.java
@@ -0,0 +1,13 @@
+package sbapiserver.ddns.net.upload_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sbapiserver/ddns/net/upload_server/UploadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sbapiserver.ddns.net.upload_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
